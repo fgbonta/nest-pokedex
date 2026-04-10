cfbf49d0a91a10a61e1c6d58b64d9c609555a21a
Fernando Bontá <fernando_bonta@epamneoris.com>
2026-04-10 16:21:07 -0300

    patron adaptador

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { InjectModel } from '@nestjs/mongoose';
+
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
-export class SeedService {
-
-  private readonly axios: AxiosInstance = axios;
+export class SeedService {  
 
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) { }
 
   async executeSeed() {
@@ -21,7 +21,7 @@ export class SeedService {
     // fetch funciona en Node.js a partir de la versión 18. 
     // Puedes instalar un paquete que proporcione la funcionalidad de fetch en Node.js, como node-fetch.
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon/?limit=650');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon/?limit=650');
 
     const pokemons: { name: string; no: number }[] = data.results.map(({ name, url }) => {
       const segments = url.split('/');
