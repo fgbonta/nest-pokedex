91f4fc782e0a8da9d0a7ae6670a841a4732f76ec
Fernando Bontá <fernando_bonta@epamneoris.com>
2026-04-23 16:19:17 -0300

    fix pagination

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,14 +1,14 @@
-import { IsNumber, IsOptional, IsPositive, Min } from "class-validator";
+import { IsInt, IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto {
 
     @IsOptional()
-    @IsNumber()
+    @IsInt()
     @IsPositive()
-    @Min(1)
     limit?: number;
 
     @IsOptional()
-    @IsNumber()
+    @IsInt()
+    @Min(0)
     offset?: number;
 }
